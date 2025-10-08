010ecefaaf737a1e399c7608c780350ef7836b80
Omar Barguti <omar.barguti@gmail.com>
2025-10-08 01:20:36 -0400

    Refactor RemoteControllerController to improve event processing and logging

--- src/spotmicroai/remote_controller/remote_controller.py
+++ src/spotmicroai/remote_controller/remote_controller.py
@@ -15,9 +15,7 @@ log = Logger().setup_logger('Remote controller')
 class RemoteControllerController:
 
     def __init__(self, communication_queues):
-
         try:
-
             log.debug('Starting controller...')
 
             signal.signal(signal.SIGINT, self.exit_gracefully)
@@ -45,11 +43,14 @@ class RemoteControllerController:
         sys.exit(0)
 
     def do_process_events_from_queues(self):
-
+        """
+        Main event loop. Reads joystick deltas but keeps publishing
+        the last known state at a steady rate so that movement persists
+        while the stick is held in position.
+        """
         remote_controller_connected_already = False
 
         while True:
-
             if self.connected_device and not remote_controller_connected_already:
                 self._lcd_screen_queue.put(queues.LCD_SCREEN_CONTROLLER_ACTION_ON)
                 self._lcd_screen_queue.put(queues.LCD_SCREEN_SHOW_REMOTE_CONTROLLER_CONTROLLER_OK)
@@ -61,187 +62,144 @@ class RemoteControllerController:
                 remote_controller_connected_already = False
                 self.check_for_connected_devices()
                 continue
-            
-            prev_evbuf = ''
+
+            PUBLISH_RATE_HZ = 20.0
+            last_publish_time = 0
+
             # Main event loop
             while True:
-
                 try:
                     evbuf = self.jsdev.read(8)
 
-                    if not evbuf:
-                        evbuf = prev_evbuf
-
                     if evbuf:
                         buftime, value, type, number = struct.unpack('IhBB', evbuf)
-                        prev_evbuf = evbuf
 
+                        # Skip initialization events
                         if type & 0x80:
                             continue
 
-                        # print(f"buftime: {buftime}, value: {value}, type: {type}, number: {number}")
-                        
+                        # Button event
                         if type & 0x01:
-
                             button = self.button_map[number]
                             if button:
                                 self.button_states[button] = value
 
-                        if type & 0x02:
+                        # Axis event
+                        elif type & 0x02:
                             axis = self.axis_map[number]
                             if axis:
                                 fvalue = round(value / 32767.0, 3)
                                 self.axis_states[axis] = fvalue
-                    states = {}
-                    states.update(self.button_states)
-                    states.update(self.axis_states)
 
-                    # log.debug(states)
+                    now = time.time()
+                    if now - last_publish_time >= 1.0 / PUBLISH_RATE_HZ:
+                        # Combine axis + button states
+                        combined = {**self.button_states, **self.axis_states}
+                        self._motion_queue.put(combined)
+                        last_publish_time = now
 
-                    self._motion_queue.put(states)
+                    time.sleep(0.005)
 
                 except Exception as e:
-                    log.error('Unknown problem while processing the queue of the remote controller controller', e)
+                    log.error('Unknown problem while processing the queue of the remote controller', e)
                     self._abort_queue.put(queues.ABORT_CONTROLLER_ACTION_ABORT)
                     remote_controller_connected_already = False
                     self.check_for_connected_devices()
                     break
 
     def check_for_connected_devices(self):
-
+        """
+        Scans /dev/input for the configured joystick device and opens it.
+        """
         connected_device = Config().get(Config.REMOTE_CONTROLLER_CONTROLLER_DEVICE)
 
         log.info('The remote controller is not detected, looking for connected devices')
         self.connected_device = False
         for fn in os.listdir('/dev/input'):
-            # if fn.startswith('js'):
             if fn.startswith(str(connected_device)):
                 self.connected_device = True
 
                 # These constants were borrowed from linux/input.h
                 axis_names = {
-                    0x00: 'lx',
-                    0x01: 'ly',
-                    0x02: 'lz',
-                    0x03: 'rx',
-                    0x04: 'ry',
-                    0x05: 'rz',
-                    0x06: 'trottle',
-                    0x07: 'rudder',
-                    0x08: 'wheel',
-                    0x09: 'gas',
-                    0x0a: 'brake',
-                    0x10: 'hat0x',
-                    0x11: 'hat0y',
-                    0x12: 'hat1x',
-                    0x13: 'hat1y',
-                    0x14: 'hat2x',
-                    0x15: 'hat2y',
-                    0x16: 'hat3x',
-                    0x17: 'hat3y',
-                    0x18: 'pressure',
-                    0x19: 'distance',
-                    0x1a: 'tilt_x',
-                    0x1b: 'tilt_y',
-                    0x1c: 'tool_width',
-                    0x20: 'volume',
+                    0x00: 'lx', 0x01: 'ly', 0x02: 'lz',
+                    0x03: 'rx', 0x04: 'ry', 0x05: 'rz',
+                    0x06: 'trottle', 0x07: 'rudder', 0x08: 'wheel',
+                    0x09: 'gas', 0x0a: 'brake',
+                    0x10: 'hat0x', 0x11: 'hat0y',
+                    0x12: 'hat1x', 0x13: 'hat1y',
+                    0x14: 'hat2x', 0x15: 'hat2y',
+                    0x16: 'hat3x', 0x17: 'hat3y',
+                    0x18: 'pressure', 0x19: 'distance',
+                    0x1a: 'tilt_x', 0x1b: 'tilt_y',
+                    0x1c: 'tool_width', 0x20: 'volume',
                     0x28: 'misc',
                 }
 
                 button_names = {
-                    0x120: 'trigger',
-                    0x121: 'thumb',
-                    0x122: 'thumb2',
-                    0x123: 'top',
-                    0x124: 'top2',
-                    0x125: 'pinkie',
-                    0x126: 'base',
-                    0x127: 'base2',
-                    0x128: 'base3',
-                    0x129: 'base4',
-                    0x12a: 'base5',
-                    0x12b: 'base6',
+                    0x120: 'trigger', 0x121: 'thumb', 0x122: 'thumb2', 0x123: 'top',
+                    0x124: 'top2', 0x125: 'pinkie', 0x126: 'base', 0x127: 'base2',
+                    0x128: 'base3', 0x129: 'base4', 0x12a: 'base5', 0x12b: 'base6',
                     0x12f: 'dead',
-                    0x130: 'a',
-                    0x131: 'b',
-                    0x132: 'c',
-                    0x133: 'x',
-                    0x134: 'y',
-                    0x135: 'z',
-                    0x136: 'tl',
-                    0x137: 'tr',
-                    0x138: 'tl2',
-                    0x139: 'tr2',
-                    0x13a: 'select',
-                    0x13b: 'start',
-                    0x13c: 'mode',
-                    0x13d: 'thumbl',
-                    0x13e: 'thumbr',
+                    0x130: 'a', 0x131: 'b', 0x132: 'c', 0x133: 'x',
+                    0x134: 'y', 0x135: 'z', 0x136: 'tl', 0x137: 'tr',
+                    0x138: 'tl2', 0x139: 'tr2', 0x13a: 'select',
+                    0x13b: 'start', 0x13c: 'mode',
+                    0x13d: 'thumbl', 0x13e: 'thumbr',
 
-                    0x220: 'dpad_up',
-                    0x221: 'dpad_down',
-                    0x222: 'dpad_left',
-                    0x223: 'dpad_right',
+                    0x220: 'dpad_up', 0x221: 'dpad_down',
+                    0x222: 'dpad_left', 0x223: 'dpad_right',
 
-                    # XBox 360 controller uses these codes.
-                    0x2c0: 'dpad_left',
-                    0x2c1: 'dpad_right',
-                    0x2c2: 'dpad_up',
-                    0x2c3: 'dpad_down',
+                    # Xbox 360 controller specific
+                    0x2c0: 'dpad_left', 0x2c1: 'dpad_right',
+                    0x2c2: 'dpad_up', 0x2c3: 'dpad_down',
                 }
 
-                # Open the joystick device.
+                # Open the joystick device
                 fn = '/dev/input/' + str(connected_device)
 
-                
                 while True:
-                    # Check if you have permissions to /dev/input/js0 yet. 
-                    # This can be an issue when running as a systemd service. The pi user may still not have access and need to wait
                     try:
-                        log.debug(('Attempting to open %s...' % fn))
+                        log.debug(f'Attempting to open {fn}...')
                         self.jsdev = open(fn, 'rb')
                         os.set_blocking(self.jsdev.fileno(), False)
-                        log.info(f'{fn } opened successfully.')
+                        log.info(f'{fn} opened successfully.')
                         break
-                    except:
+                    except Exception:
                         log.warning(f'Unable to access {fn} yet. Will retry in 5 seconds...')
                         time.sleep(2)
 
-                # Get the device name.
-                # buf = bytearray(63)
+                # Get the device name
                 buf = array.array('B', [0] * 64)
-                ioctl(self.jsdev, 0x80006a13 + (0x10000 * len(buf)), buf)  # JSIOCGNAME(len)
+                ioctl(self.jsdev, 0x80006a13 + (0x10000 * len(buf)), buf)
                 js_name = buf.tobytes().rstrip(b'\x00').decode('utf-8')
-                log.info(('Connected to device: %s' % js_name))
+                log.info(f'Connected to device: {js_name}')
 
-                # Get number of axes and buttons.
+                # Get number of axes and buttons
                 buf = array.array('B', [0])
-                ioctl(self.jsdev, 0x80016a11, buf)  # JSIOCGAXES
+                ioctl(self.jsdev, 0x80016a11, buf)
                 num_axes = buf[0]
 
                 buf = array.array('B', [0])
-                ioctl(self.jsdev, 0x80016a12, buf)  # JSIOCGBUTTONS
+                ioctl(self.jsdev, 0x80016a12, buf)
                 num_buttons = buf[0]
 
-                # Get the axis map.
+                # Get the axis map
                 buf = array.array('B', [0] * 0x40)
-                ioctl(self.jsdev, 0x80406a32, buf)  # JSIOCGAXMAP
-
+                ioctl(self.jsdev, 0x80406a32, buf)
                 for axis in buf[:num_axes]:
-                    axis_name = axis_names.get(axis, 'unknown(0x%02x)' % axis)
+                    axis_name = axis_names.get(axis, f'unknown(0x{axis:02x})')
                     self.axis_map.append(axis_name)
                     self.axis_states[axis_name] = 0.0
 
-                # Get the button map.
+                # Get the button map
                 buf = array.array('H', [0] * 200)
-                ioctl(self.jsdev, 0x80406a34, buf)  # JSIOCGBTNMAP
-
+                ioctl(self.jsdev, 0x80406a34, buf)
                 for btn in buf[:num_buttons]:
-                    btn_name = button_names.get(btn, 'unknown(0x%03x)' % btn)
+                    btn_name = button_names.get(btn, f'unknown(0x{btn:03x})')
                     self.button_map.append(btn_name)
                     self.button_states[btn_name] = 0
 
-                log.info(('%d axes found: %s' % (num_axes, ', '.join(self.axis_map))))
-                log.info(('%d buttons found: %s' % (num_buttons, ', '.join(self.button_map))))
+                log.info(f'{num_axes} axes found: {", ".join(self.axis_map)}')
+                log.info(f'{num_buttons} buttons found: {", ".join(self.button_map)}')
 
                 break
